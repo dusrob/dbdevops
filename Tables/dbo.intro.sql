7df2f04e39f342a29d02f3d5b23aa600934eb89f
dusrob <dusrob777@gmail.com>
2018-05-16 14:01:16 -0700

    asdfa

---
+++ Tables/dbo.intro.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[intro]
+(
+[firstname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[lastname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[roll] [int] NULL,
+[nickname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
