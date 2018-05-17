59b789261c8825b1fefa538758ac7cbb96ef4926
dusrob <dusrob777@gmail.com>
2018-05-17 13:57:24 -0700

    short name add

--- Tables/dbo.Table_2.sql
+++ Tables/dbo.Table_2.sql
@@ -1,5 +1,6 @@
 CREATE TABLE [dbo].[Table_2]
 (
-[name long] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[name long] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[short name] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
